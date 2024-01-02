343ea7f9cb28151299ecdefc62e9d5b2858bd634
ntmkhanh <khanhb1910657@student.ctu.edu.vn>
2024-01-02 22:41:53 +0700

    ejs

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const app = require('./src/app');
-const PORT = process.env.PORT;
-app.listen(PORT, () =>{
+
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+const PORT = process.env.PORT || 3001;
+const HOST_NAME = process.env.HOST_NAME;
+
+console.log(">>> check env: ", process.env)
+app.listen(PORT, HOST_NAME, () =>{
     console.log(`Server is running on PORT ${PORT}`)
 })
--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 app.get('/', (req, res) => {
-    res.json({message: 'Welcome to nodejs backend'})
+    res.render('index.ejs')
 })
 
 module.exports = app
