5465fde783dfc127898212a301be630caaa1b942
ntmkhanh <khanhb1910657@student.ctu.edu.vn>
2024-01-03 23:22:57 +0700

    mvc split code

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const app = require('./src/app');
+const configViewEngine = require('./src/config/viewEngine')
+const webRouter = require('./src/routes/web')
+//config
+configViewEngine(app)
 
-app.set('views', './src/views')
-app.set('view engine', 'ejs')
+//khai bao route
+app.use('/', webRouter)
 
 const PORT = process.env.PORT || 3001;
 const HOST_NAME = process.env.HOST_NAME;
 
-console.log(">>> check env: ", process.env)
+// console.log(">>> check env: ", process.env)
 app.listen(PORT, HOST_NAME, () =>{
     console.log(`Server is running on PORT ${PORT}`)
 })
--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
+app.use(cors());
+app.use(express.json());
+
+
 
 module.exports = app
